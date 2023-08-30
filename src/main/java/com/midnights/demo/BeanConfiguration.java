23f0aeacc7732004161bbb72cb47121d663f2aeb
bbbbooo <105257665+bbbbooo@users.noreply.github.com>
2023-08-30 16:53:34 +0900

    기초 설정

---
+++ src/main/java/com/midnights/demo/BeanConfiguration.java
@@ -0,0 +1,9 @@
+package com.midnights.demo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.midnights.demo")
+public class BeanConfiguration {
+}
---
+++ src/main/java/com/midnights/demo/JpaConfiguration.java
@@ -0,0 +1,11 @@
+package com.midnights.demo;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EntityScan(basePackages = "com.midnights.demo")
+@EnableJpaRepositories(basePackages = "com.midnights.demo")
+public class JpaConfiguration {
+}
